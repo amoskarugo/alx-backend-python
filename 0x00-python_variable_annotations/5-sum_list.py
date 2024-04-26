b3d580279332ceabd89ff9604a37b429513830f7
amoskarugo <karugoamos@gmail.com>
2024-04-26 14:07:49 +0300

    sum of all elements in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module containing a function to perform add operation on an iterable.
+"""
+
+lst = list[float]
+
+
+def sum_list(my_list: lst) -> float:
+    """
+    function that receives a list of floats and adds all of them together.
+    :param my_list: list containing float numbers.
+    :return: sum of all the elements in the list.
+    """
+    return sum(my_list)
