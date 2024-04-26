b215005359318d2078b544d0ea1120da0e95d648
amoskarugo <karugoamos@gmail.com>
2024-04-26 13:36:49 +0300

    function that performs and return a floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+
+"""
+module containing a function to floor a floating point number passed to it.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    annotated function floor which takes a float n as argument
+    and returns the floor of the float.
+    :param n: floating point number
+    :return: return floor of the param
+    """
+    return math.floor(n)
