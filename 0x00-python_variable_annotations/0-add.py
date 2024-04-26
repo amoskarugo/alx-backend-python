51a6559cde4ae2be73f3791eaa2f1358d8a24950
amoskarugo <karugoamos@gmail.com>
2024-04-26 13:17:03 +0300

    #

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module containing a function to add two numbers
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    function that adds two floating point numbers.
+    :param a: first parameter.
+    :param b: second parameter.
+    :return: sum of the two numbers.
+    """
+    return a + b
