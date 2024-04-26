29856dcdc440f52d1f54928a83040bb81cb4c771
amoskarugo <karugoamos@gmail.com>
2024-04-26 14:55:54 +0300

    complex type annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,10 +3,10 @@
 
 """
 import math
-from typing import Union, Tuple
+from typing import Union, Tuple, Any
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     type-annotated function to_kv that takes a string k and
     an int OR float v as arguments and returns a tuple
@@ -14,4 +14,4 @@ def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     :param v: float or int
     :return: tuple
     """
-    return k, math.pow(v, 2)
+    return k, v ** 2
