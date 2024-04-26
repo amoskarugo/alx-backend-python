f0fc9137d276a806ec3d96738f55c46410d98753
amoskarugo <karugoamos@gmail.com>
2024-04-26 13:28:14 +0300

    merge two strings, add annotations to params and return type

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+module containing a function to concatonate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    merge two strings
+    :param str1: first string
+    :param str2: second string
+    :return: merged string
+    """
+    return str1 + str2
