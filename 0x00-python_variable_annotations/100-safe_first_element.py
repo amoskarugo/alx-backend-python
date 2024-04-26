08fe7111719e3c7314c86b77ece5393576fc6c95
amoskarugo <karugoamos@gmail.com>
2024-04-26 15:54:30 +0300

    union type

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+from typing import Any, Sequence, Union
+
+
+# The types of the elements of the input are not known
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    function that returns the first element of a list
+    :param lst:  of elements
+    :return: first element or none if list is empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
