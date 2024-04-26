36a0dee5b4479fa4e5d304bfe617f8c36144afa5
amoskarugo <karugoamos@gmail.com>
2024-04-26 16:14:33 +0300

    safely get a dictionary value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ from typing import Any, Mapping, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[Any, T]):
+def safely_get_value(dct: Mapping, key: Any, default: Union[None, T]):
     """
 
     :param dct: a dictionary
