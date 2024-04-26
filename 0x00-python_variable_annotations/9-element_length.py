6de1dcb9bd1a3cc19fd4af2157c3a8b28846bddb
amoskarugo <karugoamos@gmail.com>
2024-04-26 15:38:23 +0300

    iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+"""
+module that implements a function that performs complex
+type annotation
+"""
 
 from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+
+    :param lst: an iterable object
+    :return: list of tuples
+    """
     return [(i, len(i)) for i in lst]
