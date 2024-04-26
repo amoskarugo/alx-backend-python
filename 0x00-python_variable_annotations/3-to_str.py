3bd8c0b48e4928f17eef8db28f2ecf6aeb8fdee6
amoskarugo <karugoamos@gmail.com>
2024-04-26 13:43:31 +0300

    transform float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module containing a function to floor a floating point number passed to it.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    annotated function floor which takes a float n as argument
+    and returns the string of the float.
+    :param n: floating point number
+    :return: return string of the param
+    """
+    return str(n)
