c3186aeebbb18e34ca11469f7b41f95b5fe6b38a
amoskarugo <karugoamos@gmail.com>
2024-04-27 13:51:01 +0300

    yield random numbers

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """async comprehension basics"""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float]:
     """function to yield random numbers"""
     for _ in range(10):
         yield random.random() * 10
